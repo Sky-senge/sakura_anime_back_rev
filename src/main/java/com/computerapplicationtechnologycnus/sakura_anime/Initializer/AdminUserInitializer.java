9ae0806e2c1c1c1213a98b23c4dcb247422d6ae1
Radium-bit <Radiumbit@protonmail.com>
2024-12-27 19:08:17 +0800

    给版权声明加点样式和链接

--- src/main/java/com/computerapplicationtechnologycnus/sakura_anime/Initializer/AdminUserInitializer.java
+++ src/main/java/com/computerapplicationtechnologycnus/sakura_anime/Initializer/AdminUserInitializer.java
@@ -13,11 +13,15 @@ public class AdminUserInitializer implements CommandLineRunner {
         this.userService = userService;
     }
 
+    public static final String ANSI_GREEN = "\u001B[32m";   // 绿色字体
+    public static final String ANSI_ITALIC = "\u001B[3m";  // 开启斜体
+    public static final String ANSI_RESET = "\u001B[0m";  // 重置样式
     private static final Logger logger = LoggerFactory.getLogger(AdminUserInitializer.class);
 
     @Override
     public void run(String... args) {
-        logger.info("Sakura_Anime_Backend Copyright(C)2024 Radium-bit.");
+        logger.info(ANSI_GREEN+"Sakura_Anime_Backend Copyright(C)2024 Radium-bit."+ANSI_RESET);
+        logger.info(ANSI_GREEN+ANSI_ITALIC+"Project Source: https://github.com/Radium-bit/sakura_anime_backend"+ANSI_RESET);
         // 如果有人把管理员封了或者删了，且没有其他管理员的前提下，会自动创建一个默认账户。
         logger.info("开始检查数据库状态...");
         userService.ensureAdminUser();
