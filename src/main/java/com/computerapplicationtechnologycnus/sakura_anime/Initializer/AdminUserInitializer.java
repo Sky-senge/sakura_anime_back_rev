485aa955654b4cbc0c290a624eefd5269c6fa134
Radium-bit <Radiumbit@protonmail.com>
2024-12-27 18:40:01 +0800

    添加版权声明

--- src/main/java/com/computerapplicationtechnologycnus/sakura_anime/Initializer/AdminUserInitializer.java
+++ src/main/java/com/computerapplicationtechnologycnus/sakura_anime/Initializer/AdminUserInitializer.java
@@ -17,6 +17,7 @@ public class AdminUserInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        logger.info("Sakura_Anime_Backend Copyright(C)2024 Radium-bit.");
         // 如果有人把管理员封了或者删了，且没有其他管理员的前提下，会自动创建一个默认账户。
         logger.info("开始检查数据库状态...");
         userService.ensureAdminUser();
